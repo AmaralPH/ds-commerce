e04f382594322d7431090a5345b4f864e3d481bc
pedro amaral <amaralph1995@gmail.com>
2024-12-11 10:28:02 -0300

    bean validation

--- src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -2,11 +2,20 @@ package com.devsuperior.dscommerce.dto;
 
 
 import com.devsuperior.dscommerce.entities.Product;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import org.aspectj.bridge.IMessage;
 
 public class ProductDTO {
-    private Long id;
+    @Size(min = 3, max = 80, message = "Nome precisa ter de 3 a 80")
+    @NotBlank(message = "Campo requerido")
     private String name;
+    private Long id;
+    @Size(min = 10, message = "Nome precisa ter no mínimo 10 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String description;
+    @Positive(message = "O preço deve ser positivo")
     private Double price;
     private String imgUrl;
 
